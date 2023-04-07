62ed3a28ea93e8d4a3695ba009ca39d46ffb6526
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-04-07 11:55:46 +0200

    Add files via upload

--- Ball.cpp
+++ Ball.cpp
@@ -1,12 +1,21 @@
 #include "Ball.h"
 
+bool Ball::isRandInitialized = false;
+
 double Ball::randomizeAngle()
 {
-	return 0.0;
+	if (!isRandInitialized)
+	{
+		srand(time(NULL));
+		isRandInitialized = true;
+	}
+	return (((double)rand() / (double)RAND_MAX) * 60.0) - 30.0;
 }
 
 void Ball::setAngle(double angle)
 {
+	direction.x = sin(angle);
+	direction.y = -cos(angle);
 }
 
 void Ball::manageCollisionWith(sf::RenderWindow & window)
@@ -15,6 +24,15 @@ void Ball::manageCollisionWith(sf::RenderWindow & window)
 
 Ball::Ball(int x, int y, int radius, float speed)
 {
+	position.x = x;
+	position.y = y;
+	oldPosition = position;
+	shape.setFillColor(sf::Color(sf::Color::White));
+	this->radius = radius;
+	shape.setRadius(radius);
+	this->speed = speed;
+
+	setAngle(randomizeAngle());
 }
 
 Ball::~Ball()
@@ -23,25 +41,31 @@ Ball::~Ball()
 
 void Ball::move(float ellapsedTime)
 {
+	position += ellapsedTime * speed * direction;
 }
 
 void Ball::draw(sf::RenderWindow & window)
 {
+	window.draw(shape);
 }
 
 float Ball::getSpeed()
 {
-	return 0.0f;
+	return speed;
 }
 
 void Ball::setSpeed(float newSpeed)
 {
+	speed = newSpeed;
 }
 
 void Ball::setPosition(sf::Vector2f newPosition)
 {
+	position = newPosition;
+	oldPosition = position;
 }
 
 void Ball::setDirection(sf::Vector2f newDirection)
 {
+	direction = newDirection;
 }
