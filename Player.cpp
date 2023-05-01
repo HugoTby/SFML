9411b58dae66cdbb422f6efd985518e911e551c4
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-05-01 13:46:22 +0200

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,24 +1,34 @@
 #include "Player.h"
 
-Player::Player(int y, int width, int height)
+Player::Player(int x, int y, int width, int height)
 {
+	position.x = x;
+	position.y = y;
+	oldPosition = position;
+	shape.setSize(sf::Vector2f(width, height));
+	shape.setFillColor(sf::Color(sf::Color::Red));
+	speed = 500.0f; // vitesse de déplacement
 }
 
 Player::~Player()
 {
 }
 
-void Player::draw(sf::RenderWindow & window)
+// implémenter la fonction pour dessiner le joueur
+void Player::draw(sf::RenderWindow& window)
 {
+	shape.setPosition(position);
+	window.draw(shape);
 }
 
-
-sf::Vector2f Player::getPosition()
+// implémenter la fonction pour déplacer le joueur vers la gauche
+void Player::moveLeft(float elapsedTime)
 {
-	return sf::Vector2f();
+	position.x -= speed * elapsedTime;
 }
 
-sf::Vector2f Player::getSize()
+// implémenter la fonction pour déplacer le joueur vers la droite
+void Player::moveRight(float elapsedTime)
 {
-	return sf::Vector2f();
+	position.x += speed * elapsedTime;
 }
