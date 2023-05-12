b89b2ebcdb20332537677a683b32069b5c764dca
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-05-12 09:37:16 +0200

    Update Brick.h

--- Brick.h
+++ Brick.h
@@ -1,24 +1,31 @@
 #ifndef BRICK_H
 #define BRICK_H
+#include <deque>
 
 #include <SFML/Graphics.hpp>
 
 class Brick
 {
 private:
-	sf::RectangleShape shape;
 	sf::Vector2f position;
-	sf::Vector2f size;
+	sf::Vector2f size; 
+	sf::RectangleShape shape; 
 	int health;
+	float lastCollisionTime; 
+	sf::Clock clock; 
+	void setColor();
 public:
 	Brick(int x, int y, int width, int height, int health);
-	~Brick();
-	void draw(sf::RenderWindow& window);
-	sf::Color getColorFromLife();
+	~Brick(); 
+	void draw(sf::RenderWindow& window); 
 	bool isAlive();
-	void hit();
-	sf::Vector2f getPosition();
-	sf::Vector2f getSize();
+	void destroyAndDelete(std::deque<Brick*>& bricks);
+	void hit(); 
+	sf::Vector2f getPosition(); 
+	sf::RectangleShape getShape();
+	sf::Vector2f getSize(); 
+	bool operator==(const Brick& other) const; 
+
 };
 
 #endif
