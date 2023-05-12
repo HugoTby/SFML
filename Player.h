989f6ea2080e2d41550f28c2867745eb7ee967fa
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-05-12 09:37:45 +0200

    Update Player.h

--- Player.h
+++ Player.h
@@ -4,21 +4,24 @@
 class Player
 {
 public:
-	Player(int x, int y, int width, int height); // constructeur
-	~Player(); // destructeur
-
-	// ajouter une fonction pour dessiner le joueur
+	static constexpr float PLAYER_SPEED = 600.f;
+	static constexpr int SCREEN_WIDTH = 1200;
+	Player(int y, int width, int height);
+	~Player();
 	void draw(sf::RenderWindow& window);
+	void handleKeyboard(sf::Keyboard::Key key, bool isPressed);
+	void update(float deltaTime);
+	void move(float distance);
 
-	// ajouter une fonction pour déplacer le joueur vers la gauche
-	void moveLeft(float elapsedTime);
-
-	// ajouter une fonction pour déplacer le joueur vers la droite
-	void moveRight(float elapsedTime);
+	sf::Vector2f getSurfaceNormal(sf::Vector2f point);
+	sf::RectangleShape getShape();
+	sf::Vector2f getPosition();
+	sf::Vector2f getSize();
 
 private:
-	sf::Vector2f position;
-	sf::Vector2f oldPosition;
 	sf::RectangleShape shape;
-	float speed;
+	sf::Vector2f position;
+	sf::Vector2f size;
+	int y;
+	float velocity = 0.f; 
 };
