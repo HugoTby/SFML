5e34cfc6fd05354c208af412a00ced6ff10cf650
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-05-12 09:35:51 +0200

    Update Brick.cpp

--- Brick.cpp
+++ Brick.cpp
@@ -2,30 +2,55 @@
 
 Brick::Brick(int x, int y, int width, int height, int health)
 {
+	
+	this->position = sf::Vector2f(x, y);
+	this->size = sf::Vector2f(width, height);
+	this->shape = sf::RectangleShape(size);
+	this->health = health;
+	shape.setFillColor(sf::Color::Green); 
+	this->shape.setOutlineColor(sf::Color::White);
+	this->shape.setOutlineThickness(1); 
+	this->lastCollisionTime = 0; 
 }
 
 Brick::~Brick()
 {
 }
 
-void Brick::draw(sf::RenderWindow & window)
+void Brick::draw(sf::RenderWindow& window)
 {
+	if (isAlive())
+	{
+		
+		shape.setPosition(position);
+		window.draw(shape);
+	}
 }
-
-sf::Color Brick::getColorFromLife()
-{
-	return sf::Color();
-}
-
 bool Brick::isAlive()
 {
-	return false;
+	
+	return health > 0;
+}
+
+void Brick::destroyAndDelete(std::deque<Brick*>& bricks)
+{
+	bricks.erase(std::remove(bricks.begin(), bricks.end(), this), bricks.end());
+
+	delete this;
 }
 
 void Brick::hit()
 {
-}
+	if (isAlive())
+	{
 
+		if (clock.getElapsedTime().asSeconds() - lastCollisionTime >= 0.1) {
+			health--;
+			setColor();
+			lastCollisionTime = clock.getElapsedTime().asSeconds();
+		}
+	}
+}
 sf::Vector2f Brick::getPosition()
 {
 	return sf::Vector2f();
@@ -34,4 +59,30 @@ sf::Vector2f Brick::getPosition()
 sf::Vector2f Brick::getSize()
 {
 	return sf::Vector2f();
+}
+
+sf::RectangleShape Brick::getShape()
+{
+	
+	return shape;
+}
+
+void Brick::setColor()
+{
+	this->shape.setOutlineColor(sf::Color::White); 
+	this->shape.setOutlineThickness(1); 
+	if (this->health == 2)
+	{
+		this->shape.setFillColor(sf::Color(255, 165, 0)); 
+	}
+	else if (this->health == 1)
+	{
+
+		this->shape.setFillColor(sf::Color::Red); 
+	}
+}
+
+bool Brick::operator==(const Brick& other) const
+{
+	return this == &other;
 }
