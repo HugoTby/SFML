6cb88725147b22008d11cd3236d018ff6f80d943
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-05-12 09:34:35 +0200

    Update Ball.h

--- Ball.h
+++ Ball.h
@@ -2,11 +2,13 @@
 #define BALL_H
 
 #include <SFML/Graphics.hpp>
+#include "Player.h"
+#include "Brick.h"
 
 class Ball
 {
 private:
-	sf::CircleShape shape; // Dessiner la balle sur l'écran
+	sf::CircleShape shape; // Dessiner la balle sur l'Ã©cran
 	sf::Vector2f oldPosition;
 	sf::Vector2f position;
 	float speed;
@@ -15,8 +17,9 @@ private:
 	static bool isRandInitialized;
 	double randomizeAngle();
 	void setAngle(double angle);
+	int lives;
 
-	
+
 
 	//void manageCollisionWith(Player& player); //Collisions plateforme
 
@@ -25,11 +28,17 @@ public:
 	~Ball();
 	void move(float ellapsedTime);
 	void draw(sf::RenderWindow& window);
-	float getSpeed();
+
+	float getSpeed() const;
 	void setSpeed(float newSpeed);
+
 	void setPosition(sf::Vector2f newPosition);
+	sf::Vector2f getPosition() const;
 	void setDirection(sf::Vector2f newDirection);
-	void manageCollisionWith(sf::RenderWindow& window); //Collisions bordures
+	int getLives() const;
+	void manageCollisionWithWindow(const sf::RenderWindow& window);
+	void manageCollisionWithPlayer(Player& player);
+	void manageCollisionWithBrick(Brick* brick);
 };
 
 #endif // BALL_H
